ce21ac1f6a00565f028aad51e4286ec02373764e
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-02-25 05:05:03 +0700

    fix layput col

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,9 +45,9 @@
                     @endauth
                 </ul>
             </div>
-            <a href="/" class="text-xl font-bold text-brand flex items-center gap-2 lg:ml-0">
+            <a href="/" class="text-xl font-bold text-brand flex items-center gap-2 lg:ml-0 whitespace-nowrap">
                 <x-icon name="o-megaphone" class="w-6 h-6" />
-                Kembaran Ngadu
+                <span class="hidden lg:block">Kembaran Ngadu</span>
             </a>
         </div>
 
